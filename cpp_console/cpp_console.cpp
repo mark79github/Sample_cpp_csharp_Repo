4ea52d4b24fddef6fd4defdd276880aa81111285
Ravi Kumar M <41356942+mark79github@users.noreply.github.com>
2022-02-26 15:06:57 +0530

    Add project files.

---
+++ cpp_console/cpp_console.cpp
@@ -0,0 +1,13 @@
+﻿// cpp_console.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hi" << endl;
+    return 0;
+}
+
---
+++ csharp_console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_console
+{
+    class Program
+    {
+        class A
+        {
+            static int i;
+            static A()
+            {
+                Console.WriteLine("Static Constructor of A has No Control. Executes before any intance is created.");
+                i = 5;
+            }
+            public A() { Console.WriteLine("default constructor of A"); }
+            public void show()
+            { Console.WriteLine("value of i is : " + i); }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is main");
+            A a = new A();
+            a.show();
+        }
+    }
+}
